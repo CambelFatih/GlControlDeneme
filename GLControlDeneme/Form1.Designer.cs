ceddf86a02e62a03841430bf84f45de977e45ff5
Fatih Cambel <cambelfatih01@gmail.com>
2021-03-17 18:31:51 +0300

    Add Project Files

---
+++ GLControlDeneme/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace GLControlDeneme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.glControl1 = new OpenTK.GLControl();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnY = new System.Windows.Forms.Button();
+            this.btnX = new System.Windows.Forms.Button();
+            this.btnZ = new System.Windows.Forms.Button();
+            this.timerX = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(12, 16);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(442, 456);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(498, 54);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(517, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(517, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(482, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(482, 283);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(482, 329);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // btnY
+            // 
+            this.btnY.Location = new System.Drawing.Point(549, 278);
+            this.btnY.Name = "btnY";
+            this.btnY.Size = new System.Drawing.Size(75, 23);
+            this.btnY.TabIndex = 8;
+            this.btnY.Text = "y";
+            this.btnY.UseVisualStyleBackColor = true;
+            this.btnY.Click += new System.EventHandler(this.btnY_Click);
+            // 
+            // btnX
+            // 
+            this.btnX.Location = new System.Drawing.Point(549, 228);
+            this.btnX.Name = "btnX";
+            this.btnX.Size = new System.Drawing.Size(75, 23);
+            this.btnX.TabIndex = 9;
+            this.btnX.Text = "x";
+            this.btnX.UseVisualStyleBackColor = true;
+            this.btnX.Click += new System.EventHandler(this.btnX_Click);
+            this.btnX.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnX_KeyDown);
+            this.btnX.KeyUp += new System.Windows.Forms.KeyEventHandler(this.btnX_KeyUp);
+            // 
+            // btnZ
+            // 
+            this.btnZ.Location = new System.Drawing.Point(549, 329);
+            this.btnZ.Name = "btnZ";
+            this.btnZ.Size = new System.Drawing.Size(75, 23);
+            this.btnZ.TabIndex = 8;
+            this.btnZ.Text = "z";
+            this.btnZ.UseVisualStyleBackColor = true;
+            // 
+            // timerX
+            // 
+            this.timerX.Tick += new System.EventHandler(this.timerX_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 502);
+            this.Controls.Add(this.btnX);
+            this.Controls.Add(this.btnZ);
+            this.Controls.Add(this.btnY);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnY;
+        private System.Windows.Forms.Button btnX;
+        private System.Windows.Forms.Button btnZ;
+        private System.Windows.Forms.Timer timerX;
+    }
+}
+
---
+++ GLControlDeneme/Form1.cs
@@ -0,0 +1,430 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System.IO.Ports;
+
+namespace GLControlDeneme
+{
+    public partial class Form1 : Form
+    {
+        float x=0,y=0,z=0;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.BaudRate = 115200;
+            timer1.Interval = 100;
+            string[] portlar = SerialPort.GetPortNames();
+            foreach (string portAdi in portlar)
+            {
+                comboBox1.Items.Add(portAdi);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(Color.FromArgb(143, 212, 150));//();
+            button2.Enabled = false;
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            GL.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+            GL.Enable(EnableCap.DepthTest);//sonradan yazdık
+        }
+        private void silindir(float step , float topla , float radius,float dikey1,float dikey2)
+        {
+            float eski_step = 0.1f;
+            GL.Begin(BeginMode.Quads);//Y EKSEN CIZIM DAİRENİN
+            while (step <= 360)
+            {
+                if (step < 45)
+                    GL.Color3(Color.FromArgb(255,0,0));
+                else if (step < 90)
+                    GL.Color3(Color.FromArgb(255, 255, 255));
+                else if (step < 135)
+                    GL.Color3(Color.FromArgb(255, 0, 0));
+                else if (step < 180)
+                    GL.Color3(Color.FromArgb(255, 255, 255));
+                else if (step < 225)
+                    GL.Color3(Color.FromArgb(255, 0, 0));
+                else if (step < 270)
+                    GL.Color3(Color.FromArgb(255, 255, 255));
+                else if (step < 315)
+                    GL.Color3(Color.FromArgb(255, 0, 0));
+                else if (step < 360)
+                    GL.Color3(Color.FromArgb(255, 255, 255));
+
+
+                float ciz1_x = (float)(radius * Math.Cos(step * Math.PI / 180F));
+                float ciz1_y = (float)(radius * Math.Sin(step * Math.PI / 180F));
+                GL.Vertex3(ciz1_x, dikey1, ciz1_y);
+
+                float ciz2_x = (float)(radius * Math.Cos((step + 2) * Math.PI / 180F));
+                float ciz2_y = (float)(radius * Math.Sin((step + 2) * Math.PI / 180F));
+                GL.Vertex3(ciz2_x, dikey1, ciz2_y);
+
+                GL.Vertex3(ciz1_x, dikey2, ciz1_y);
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+                step += topla;
+            }
+            GL.End();
+            GL.Begin(BeginMode.Lines);
+            step = eski_step;
+            topla = step;
+            while (step <= 180)// UST KAPAK
+            {
+                if (step < 45)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 90)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 135)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 180)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 225)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 270)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 315)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 360)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+
+
+                float ciz1_x = (float)(radius * Math.Cos(step * Math.PI / 180F));
+                float ciz1_y = (float)(radius * Math.Sin(step * Math.PI / 180F));
+                GL.Vertex3(ciz1_x, dikey1, ciz1_y);
+
+                float ciz2_x = (float)(radius * Math.Cos((step + 180) * Math.PI / 180F));
+                float ciz2_y = (float)(radius * Math.Sin((step + 180) * Math.PI / 180F));
+                GL.Vertex3(ciz2_x, dikey1, ciz2_y);
+
+                GL.Vertex3(ciz1_x, dikey1, ciz1_y);
+                GL.Vertex3(ciz2_x, dikey1, ciz2_y);
+                step += topla;
+            }
+            step = eski_step;
+            topla = step;
+            while (step <= 180)//ALT KAPAK
+            {
+                if (step < 45)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 90)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 135)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 180)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 225)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 270)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 315)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 360)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+
+                float ciz1_x = (float)(radius * Math.Cos(step * Math.PI / 180F));
+                float ciz1_y = (float)(radius * Math.Sin(step * Math.PI / 180F));
+                GL.Vertex3(ciz1_x, dikey2, ciz1_y);
+
+                float ciz2_x = (float)(radius * Math.Cos((step + 180) * Math.PI / 180F));
+                float ciz2_y = (float)(radius * Math.Sin((step + 180) * Math.PI / 180F));
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+
+                GL.Vertex3(ciz1_x, dikey2, ciz1_y);
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+                step += topla;
+            }
+            GL.End();
+        }
+        private void koni(float step, float topla, float radius1,float radius2 ,float dikey1, float dikey2)
+        {
+            float eski_step = 0.1f;
+            GL.Begin(BeginMode.Lines);//Y EKSEN CIZIM DAİRENİN
+            while (step <= 360)
+            {
+                if (step < 45)
+                    GL.Color3(1.0, 1.0, 1.0);
+                else if (step < 90)
+                    GL.Color3(1.0, 0.0, 0.0);
+                else if (step < 135)
+                    GL.Color3(1.0, 1.0, 1.0);
+                else if (step < 180)
+                    GL.Color3(1.0, 0.0, 0.0);
+                else if (step < 225)
+                    GL.Color3(1.0, 1.0, 1.0);
+                else if (step < 270)
+                    GL.Color3(1.0, 0.0, 0.0);
+                else if (step < 315)
+                    GL.Color3(1.0, 1.0, 1.0);
+                else if (step < 360)
+                    GL.Color3(1.0, 0.0, 0.0);
+
+
+                float ciz1_x = (float)(radius1 * Math.Cos(step * Math.PI / 180F));
+                float ciz1_y = (float)(radius1 * Math.Sin(step * Math.PI / 180F));
+                GL.Vertex3(ciz1_x, dikey1, ciz1_y);
+
+                float ciz2_x = (float)(radius2 * Math.Cos(step  * Math.PI / 180F));
+                float ciz2_y = (float)(radius2 * Math.Sin(step  * Math.PI / 180F));
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+                step += topla;
+            }
+            GL.End();
+            
+            GL.Begin(BeginMode.Lines);
+            step = eski_step;
+            topla = step;
+            while (step <= 180)// UST KAPAK
+            {
+                if (step < 45)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 90)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 135)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 180)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 225)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 270)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+                else if (step < 315)
+                    GL.Color3(Color.FromArgb(255, 1, 1));
+                else if (step < 360)
+                    GL.Color3(Color.FromArgb(250, 250, 200));
+
+
+                float ciz1_x = (float)(radius2 * Math.Cos(step * Math.PI / 180F));
+                float ciz1_y = (float)(radius2 * Math.Sin(step * Math.PI / 180F));
+                GL.Vertex3(ciz1_x, dikey2, ciz1_y);
+
+                float ciz2_x = (float)(radius2 * Math.Cos((step + 180) * Math.PI / 180F));
+                float ciz2_y = (float)(radius2 * Math.Sin((step + 180) * Math.PI / 180F));
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+
+                GL.Vertex3(ciz1_x, dikey2, ciz1_y);
+                GL.Vertex3(ciz2_x, dikey2, ciz2_y);
+                step += topla;
+            }
+            step = eski_step;
+            topla = step;
+            GL.End();
+        }
+        private void Pervane(float yukseklik,float uzunluk,float kalinlik,float egiklik)
+        {
+            float radius=10, angle=45.0f;
+            GL.Begin(BeginMode.Quads);
+          
+            GL.Color3(Color.Red);
+            GL.Vertex3(uzunluk, yukseklik, kalinlik);
+            GL.Vertex3(uzunluk, yukseklik+ egiklik, -kalinlik);
+            GL.Vertex3(0.0, yukseklik+ egiklik, -kalinlik);
+            GL.Vertex3(0.0, yukseklik, kalinlik);
+
+            GL.Color3(Color.Red);
+            GL.Vertex3(-uzunluk, yukseklik+ egiklik, kalinlik);
+            GL.Vertex3(-uzunluk, yukseklik, -kalinlik);
+            GL.Vertex3( 0.0, yukseklik , -kalinlik);
+            GL.Vertex3( 0.0, yukseklik+ egiklik, kalinlik);
+
+            GL.Color3(Color.White);
+            GL.Vertex3(kalinlik, yukseklik, -uzunluk);
+            GL.Vertex3(-kalinlik, yukseklik + egiklik, -uzunluk);
+            GL.Vertex3(-kalinlik, yukseklik + egiklik, 0.0);//+
+            GL.Vertex3(kalinlik, yukseklik, 0.0);//-
+
+            GL.Color3(Color.White);
+            GL.Vertex3(kalinlik, yukseklik+ egiklik, +uzunluk);
+            GL.Vertex3(-kalinlik, yukseklik , +uzunluk);
+            GL.Vertex3(-kalinlik, yukseklik , 0.0);
+            GL.Vertex3(kalinlik, yukseklik+ egiklik, 0.0);
+
+            /*
+            float ciz1_x = (float)(radius * Math.Cos(angle * Math.PI / 180F));
+            float ciz1_z = (float)(radius * Math.Sin(angle * Math.PI / 180F));
+            GL.Color3(Color.Black);
+            GL.Vertex3(ciz1_x-0.5, 6.0, ciz1_z+ 0.5);
+            GL.Vertex3(ciz1_x+ 0.5, 5.0, ciz1_z-0.5);
+            GL.Vertex3(0.5, 5.0, -0.5);
+            GL.Vertex3(-0.5, 6.0, 0.5);*/
+            GL.End();
+
+        }
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            float step = 1.0f;
+            float topla = step;
+            float radius = 5.0f;
+            float dikey1 = radius,dikey2=-radius;
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(1.04f,4/3,1, 10000);
+            Matrix4 lookat = Matrix4.LookAt(25, 0, 0, 0, 0, 0, 0, 1, 0);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.LoadMatrix(ref perspective);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.LoadMatrix(ref lookat);
+            GL.Viewport(0, 0, glControl1.Width, glControl1.Height);
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+
+            GL.Rotate(x, 1.0, 0.0, 0.0);//ÖNEMLİ
+            GL.Rotate(z, 0.0, 1.0, 0.0);
+            GL.Rotate(y, 0.0, 0.0, 1.0);
+
+            silindir(step,topla,radius,3,-5);
+            silindir(0.01f, topla, 0.5f, 9, 9.7f);
+            silindir(0.01f, topla, 0.1f, 5, dikey1 + 5);
+            koni(0.01f, 0.01f, radius, 3.0f, 3, 5);
+            koni(0.01f, 0.01f, radius ,2.0f, -5.0f, -10.0f);
+            Pervane(9.0f,11.0f,0.2f,0.5f);
+
+            GL.Begin(BeginMode.Lines);
+
+            GL.Color3(Color.FromArgb(250,0,0));
+            GL.Vertex3(-30.0, 0.0, 0.0);
+            GL.Vertex3(30.0, 0.0, 0.0);
+            
+            
+            GL.Color3(Color.FromArgb(0,0,0));
+            GL.Vertex3(0.0, 30.0, 0.0);
+            GL.Vertex3(0.0, -30.0, 0.0);
+
+            GL.Color3(Color.FromArgb(0, 0, 250));
+            GL.Vertex3(0.0, 0.0, 30.0);
+            GL.Vertex3(0.0, 0.0, -30.0);
+
+            GL.End();
+            //GraphicsContext.CurrentContext.VSync = true;
+            glControl1.SwapBuffers();
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            glControl1.Invalidate();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            glControl1.Invalidate();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string sonuc = serialPort1.ReadLine();
+            string[] paket = sonuc.Split('*');
+            
+
+            try
+            {
+                string[] dn = paket[0].Split('.');
+                string[] dn2 = paket[1].Split('.');
+                string[] dn3 = paket[2].Split('.');
+
+
+                x = Convert.ToInt16(dn[0]);  
+                y = Convert.ToInt16(dn2[0]);  
+                z = Convert.ToInt16(dn3[0]); 
+                glControl1.Invalidate();
+
+                label1.Text = dn[0];
+                label2.Text = dn2[0];
+                label3.Text = dn3[0];
+            }
+            catch
+            {
+
+            }
+            
+            
+
+
+            //serialPort1.DiscardInBuffer();
+        }
+
+        private void btnX_Click(object sender, EventArgs e)
+        {
+            x = 100;
+            y = 20;
+            z = 50;
+            glControl1.Invalidate();
+            timerX.Start();
+        }
+
+        private void btnX_KeyDown(object sender, KeyEventArgs e)
+        {
+            timerX.Start();
+        }
+
+        private void btnX_KeyUp(object sender, KeyEventArgs e)
+        {
+           // timerX.Stop();
+        }
+
+        private void timerX_Tick(object sender, EventArgs e)
+        {
+            if (x < 180)
+                x += 1;
+            else
+                x = 0;
+            label1.Text = x.ToString();
+            glControl1.Invalidate();
+        }
+
+        private void btnY_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox1.Text;
+                if (!serialPort1.IsOpen)
+                {
+                    timer1.Start();
+                    serialPort1.Open();
+                    MessageBox.Show("BAĞLANTI KURULDU");
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("BAĞLANTI KURULAMADI");
+                button1.Enabled = true;
+                button2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.DiscardInBuffer();
+            serialPort1.Close();
+            timer1.Stop();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            MessageBox.Show("BAĞLANTI KESİLDİ");
+        }
+    }
+}
